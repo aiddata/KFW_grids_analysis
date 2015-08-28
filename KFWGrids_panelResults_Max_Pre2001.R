3fb9bc74ab3641ea9d0522eb6124ba34841d39a3
rtrichler <rbtrichler@Rachels-MacBook-Pro-3.local>
2015-08-27 21:04:20 -0400

    rename pre-trends

--- KFWGrids_panelResults_Max_Pre2001.R
+++ KFWGrids_panelResults_Max_Pre2001.R
@@ -21,6 +21,15 @@ loadLibs()
 shpfile = "/Users/rbtrichler/Documents/AidData/KFW Brazil Eval/GridDataProcessed/OhFive_gridanalysis_inputs_wpretrends.shp"
 dta_Shp = readShapePoly(shpfile)
 
+
+names(dta_Shp@data)[names(dta_Shp@data)=="pre_trend_"] <- "pre_trend_NDVI_max"
+names(dta_Shp@data)[names(dta_Shp@data)=="pre_trend_.1"] <- "pre_trend_temp_mean"
+names(dta_Shp@data)[names(dta_Shp@data)=="pre_trend_.2"] <- "pre_trend_temp_max"
+names(dta_Shp@data)[names(dta_Shp@data)=="pre_trend_.3"] <- "pre_trend_temp_min"
+names(dta_Shp@data)[names(dta_Shp@data)=="pre_trend_.4"] <- "pre_trend_precip_mean"
+names(dta_Shp@data)[names(dta_Shp@data)=="pre_trend_.5"] <- "pre_trend_precip_max"
+names(dta_Shp@data)[names(dta_Shp@data)=="pre_trend_.6"] <- "pre_trend_precip_min"
+
 #-------------------------------------------------
 #-------------------------------------------------
 #Pre-processing to create cross-sectional variable summaries
@@ -71,12 +80,15 @@ dta_Shp = readShapePoly(shpfile)
 dta_Shp@data["TrtBin"] <- 0
 dta_Shp@data$TrtBin[dta_Shp@data$demend_y <= 2001] <- 1
 dta_Shp@data$TrtBin[(dta_Shp@data$demend_m > 4) & (dta_Shp@data$demend_y==2001)] <- 0
+trttable <- table (dta_Shp@data$TrtBin)
+View(trttable)
 
 #Remove units that did not ever receive any treatment (within-sample test)
 dta_Shp@data$NA_check <- 0
 dta_Shp@data$NA_check[is.na(dta_Shp@data$demend_y)] <- 1
 int_Shp <- dta_Shp[dta_Shp@data$NA_check != 1,]
 dta_Shp <- int_Shp
+
 
 
 #-------------------------------------------------
@@ -97,7 +109,7 @@ psmRes <- SAT::SpatialCausalPSM(dta_Shp,mtd="logit",psmModel,drop="support",visu
 #-------------------------------------------------
 #-------------------------------------------------
 drop_set<- c(drop_unmatched=TRUE,drop_method="None",drop_thresh=0.25)
-psm_Pairs <- SAT(dta = psmRes$data, mtd = "fastNN",constraints=c(groups="UF"),psm_eq = psmModel, ids = "id", drop_opts = drop_set, visual="TRUE", TrtBinColName="TrtBin")
+psm_Pairs <- SAT(dta = psmRes$data, mtd = "fastNN",constraints=c(groups="UF"),psm_eq = psmModel, ids = "GridID", drop_opts = drop_set, visual="TRUE", TrtBinColName="TrtBin")
 #c(groups=c("UF"),distance=NULL)
 trttable <- table (psm_Pairs@data$TrtBin)
 View(trttable)
@@ -113,13 +125,13 @@ View(trttable)
 #psm_Pairs$enforce_st[psm_Pairs$enforce_st == "1998-1999"] <- NA
 #psm_Pairs$enforce_st <- as.numeric(paste(psm_Pairs$enforce_st))
 
-varList = c("MeanL_","MaxL_")
+varList = c("MaxL_")
 psm_Long <- BuildTimeSeries(dta=psm_Pairs,idField="GridID",varList_pre=varList,1982,2010,colYears=c("demend_y","apprend_y","regend_y"),interpYears=c("Slope","Road_dist","Riv_Dist","UF","Elevation","terrai_are","Pop_","MeanT_","MeanP_","MaxT_","MaxP_","MinP_","MinT_"))
 psm_Long$Year <- as.numeric(psm_Long$Year)
 
 
 
-pModelMax_A <- "MaxL_ ~ TrtMnt_regend_y + factor(reu_id) "
+pModelMax_A <- "MaxL_ ~ TrtMnt_demend_y + factor(GridID) "
 pModelMax_B <- "MaxL_ ~ TrtMnt_regend_y + MeanT_ + MeanP_ + Pop_ + MaxT_ + MaxP_ + MinT_ + MinP_  + factor(reu_id) "
 pModelMax_C <- "MaxL_ ~ TrtMnt_regend_y + MeanT_ + MeanP_ + Pop_ + MaxT_ + MaxP_ + MinT_ + MinP_  + factor(reu_id) + Year"
 
