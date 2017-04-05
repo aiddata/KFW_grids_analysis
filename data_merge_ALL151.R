1eb18e91c37690de95583f883bba1ed0629da973
rtrichler <rbtrichler@ip-128-239-131-81.v4.wm.edu>
2017-04-05 15:48:59 -0400

    code to build panel dataset, update shpfile merge code

--- data_merge_ALL151.R
+++ data_merge_ALL151.R
@@ -32,9 +32,10 @@ kfw_grid0 <- kfw_grid[kfw_grid$NA_check != 1,]
 
 #Merge grid-level covariate files
 #elevation
+#since dropped out non-PPTAL communities, there will be many records that can't be matched in merge (81366 records)
 kfw_grid_elevation <- read.csv("/Users/rbtrichler/Documents/AidData/KFW Brazil Eval/Grid Data Extracts/KFW_Grids/extracted_data/elevation/extract.csv")
 names(kfw_grid_elevation)[2] = "Elevation"
-kfw_grid1= merge(kfw_grid, kfw_grid_elevation, by.x="GridID", by.y="Id")
+kfw_grid1= merge(kfw_grid0, kfw_grid_elevation, by.x="GridID", by.y="Id")
 
 #nighttime lights
 kfw_grid_lights <- read.csv("/Users/rbtrichler/Documents/AidData/KFW Brazil Eval/Grid Data Extracts/KFW_Grids/extracted_data/v4avg_lights_x_pct/extract_merge.csv")
@@ -213,12 +214,12 @@ kfw_grid_pop<-kfw_grid_pop[,-grep("(Pop_95.2000)",names(kfw_grid_pop))]
 kfw_grid_pop<-kfw_grid_pop[,-grep("(Pop_2000.05)",names(kfw_grid_pop))]
 kfw_grid_pop<-kfw_grid_pop[,-grep("(Pop_2005.10)",names(kfw_grid_pop))]
 
-kfw_grid13.1=merge(kfw_grid21, kfw_grid_pop, by="GridID")
+kfw_grid13.1=merge(kfw_grid13, kfw_grid_pop, by="GridID")
 
 kfw_grid13<-kfw_grid13.1
 
 ##Write dataset prior to merging in high pressure covars and creating pre-trends
-#writePolyShape(kfw_grid13,"OhFive_gridanalysis_inputs_ALL151.shp")
+writePolyShape(kfw_grid13,"/Users/rbtrichler/Documents/AidData/KFW Brazil Eval/GridDataProcessed/OhFive_gridanalysis_inputs_ALL151.shp")
 
 ##---
 #Merge in high pressure covars (from Ash) at the community level: Vegetation, Soil Type Zones
@@ -262,7 +263,7 @@ kfw_grid_rail<-read.csv("/Users/rbtrichler/Documents/AidData/KFW Brazil Eval/Gri
 names(kfw_grid_rail)[2]="rail_dist"
 kfw_grid20=merge(kfw_grid19, kfw_grid_rail, by.x="GridID", by.y="Id")
 
-## Eliminate non-PPTAL communities
+## Eliminate non-PPTAL demarcated communities
 
 # kfw_grid20$NA_check <- 0
 # kfw_grid20$NA_check[is.na(kfw_grid20$demend_y)] <- 1
@@ -276,9 +277,9 @@ kfw_grid20$pt_temp_mean <- timeRangeTrend(kfw_grid20,"MeanT_[0-9][0-9][0-9][0-9]
 kfw_grid20$pt_temp_max <- timeRangeTrend(kfw_grid20,"MaxT_[0-9][0-9][0-9][0-9]",1982,1995,"GridID")
 kfw_grid20$pt_temp_min <- timeRangeTrend(kfw_grid20,"MinT_[0-9][0-9][0-9][0-9]",1982,1995,"GridID")
 
-kfw_grid20$pt_precip_mean <- timeRangeTrend(kfw_grid20,"MeanP_[0-9][0-9][0-9][0-9]",1982,1995,"GridID")
-kfw_grid20$pt_precip_max <- timeRangeTrend(kfw_grid20,"MaxP_[0-9][0-9][0-9][0-9]",1982,1995,"GridID")
-kfw_grid20$pt_min <- timeRangeTrend(kfw_grid20,"MinP_[0-9][0-9][0-9][0-9]",1982,1995,"GridID")
+kfw_grid20$pt_prec_mean <- timeRangeTrend(kfw_grid20,"MeanP_[0-9][0-9][0-9][0-9]",1982,1995,"GridID")
+kfw_grid20$pt_prec_max <- timeRangeTrend(kfw_grid20,"MaxP_[0-9][0-9][0-9][0-9]",1982,1995,"GridID")
+kfw_grid20$pt_prec_min <- timeRangeTrend(kfw_grid20,"MinP_[0-9][0-9][0-9][0-9]",1982,1995,"GridID")
 
 kfw_grid20$pt_ntl <- timeRangeTrend(kfw_grid20,"ntl_[0-9][0-9][0-9][0-9]",1992,1995,"GridID")
 
@@ -294,89 +295,8 @@ kfw_grid20$pt_wv <- timeRangeTrend(kfw_grid20,"wv[0-9][0-9][0-9][0-9]",1994,1995
 
 
 ## Write Final Shapefile, with pre-trends
-writePolyShape(kfw_grid20,"OhFive_gridanalysis_inputs_pretrends_ALL151.shp")
+writePolyShape(kfw_grid20,"/Users/rbtrichler/Documents/AidData/KFW Brazil Eval/GridDataProcessed/OhFive_gridanalysis_inputs_pretrends_ALL151.shp")
 
-#kfw_grid20 = readShapePoly("OhFive_gridanalysis_inputs_ALL151.shp")
-
-#-------------------------
-##Convert to panel dataset
-#-------------------------
-
-#Drop data for unused years (after 2010) in order to allow "reshape" to work
-kfw_grid_reshape <- kfw_grid20
-kfw_grid_reshape1<-kfw_grid_reshape[,-grep("(2011)",names(kfw_grid_reshape))]
-kfw_grid_reshape2<-kfw_grid_reshape1[,-grep("(2012)",names(kfw_grid_reshape1))]
-kfw_grid_reshape3<-kfw_grid_reshape2[,-grep("(2013)",names(kfw_grid_reshape2))]
-kfw_grid_reshape4<-kfw_grid_reshape3[,-grep("(2014)",names(kfw_grid_reshape3))]
-kfw_grid_reshape5<-kfw_grid_reshape4[,-grep("(1981)",names(kfw_grid_reshape4))]
-
-kfw_grid_reshape5<-kfw_grid_reshape5[,order(names(kfw_grid_reshape5))]
-
-MeanT<-grep("MeanT_",names(kfw_grid_reshape5))
-MeanP<-grep("MeanP_",names(kfw_grid_reshape5))
-MinT<-grep("MinT_",names(kfw_grid_reshape5))
-MaxT<-grep("MaxT_",names(kfw_grid_reshape5))
-MinP<-grep("MinP_",names(kfw_grid_reshape5))
-MaxP<-grep("MaxP_",names(kfw_grid_reshape5))
-MaxL<-grep("MaxL_",names(kfw_grid_reshape5))
-Pop<-grep("Pop_",names(kfw_grid_reshape5))
-
-all_reshape <- c(MeanT,MeanP,MaxT,MaxP,MinP,MinT,MaxL,Pop)
-psm_Long <- reshape(kfw_grid_reshape5@data, varying=all_reshape, direction="long",idvar="GridID",sep="_",timevar="Year")
-
-#Create years to demarcation
-psm_Long$yrtodem <- NA
-psm_Long$yrtodem=psm_Long$Year - psm_Long$demend_y
-
-#Create demarcation treatment variable, using demend_y
-#0 in years prior to demarcation, turns to 1 in year of demarcation
-psmtest3 <- psm_Long
-psmtest3$trtdem <- 0
-psmtest3$trtdem[which(psmtest3$Year<psmtest3$demend_y)]<-0
-psmtest3$trtdem[which(psmtest3$Year>=psmtest3$demend_y)]<-1
-
-psm_Long <- psmtest3
-
-#Add enforcement start year
-
-#correct communities with enforcement prior to demarcation
-psmtest5 <- psm_Long
-psmtest5$enfdiff= psmtest5$enforce_st - psmtest5$demend_y
-summary(psmtest5$enfdiff)
-psmenf <- subset(psmtest5, psmtest5$enfdiff<0)
-table(psmenf$reu_id)
-
-psmtest5$enforce_st[which(psmtest5$reu_id==84)]<-2007
-
-#create enforcement treatment var
-psmtest5$trtenf <- 0
-psmtest5$trtenf[which(psmtest5$Year>=psmtest5$enforce_st)]<-1
-table(psmtest5$trtenf)
-
-psm_Long <- psmtest5
+#kfw_grid20 = readShapePoly("OhFive_gridanalysis_inputs_pretrends_ALL151.shp")
 
 
-write.csv(psm_Long,file="psm_Long_ALL151.csv")
-
-##Scratch
-
-sub29<-kfw_grid_reshape5[kfw_grid_reshape5@data$GridID==225929,]
-View(sub29@data)
-View(sub29@data[,(100:200)])
-View(sub29@data[,(200:296)])
-
-#----
-#Old code to build panel data using SCI
-##****WRONG!!****** 
-
-# varList=c("MaxL_")
-# psm_Long <- BuildTimeSeries(dta=kfw_grid20,idField="GridID",varList_pre=varList,1982,2010,colYears=c("demend_y","apprend_y","regend_y"),
-#                             interpYears=c("Slope","Road_dist","Riv_Dist","UF","Elevation","terrai_are","Pop_","MeanT_","MeanP_","MaxT_",
-#                                           "MaxP_","MinP_","MinT_","ntl_",
-#                                           "urbtravtim","reu_id", "Id" ))
-# 
-# all_reshape <- c(PCloss, mean_ln, minairTemp, maxairTemp, meanairTemp, minPre, maxPre, meanPre, MinDist, DecayDist, ProjCount, DecayDist100, DecayDist25)
-# DFa4 <- reshape(DFa3, varying=all_reshape,direction="long", idvar="ID", sep="_", timevar="Year")
-# 
-# 
-# psm_Long$Year <- as.numeric(psm_Long$Year)
